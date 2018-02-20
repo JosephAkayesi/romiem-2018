76f93f7cc01c0079c5cbb99d0bef780de81360bd
Rohit Mistry <romiem@Rohits-MacBook.local>
2018-02-20 22:55:48 +0000

    Fixed social links

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -6,9 +6,9 @@ import { faTwitter, faLinkedin, faDribbble, faGithub, faTwitch } from '@fortawes
 const Social = () => (
   <div className="Social">
     <a href="https://twitter.com/romiem"><FontAwesomeIcon icon={faTwitter}/></a>
-    <a href="https://twitter.com/romiem"><FontAwesomeIcon icon={faLinkedin}/></a>
-    <a href="https://twitter.com/romiem"><FontAwesomeIcon icon={faDribbble}/></a>
-    <a href="https://twitter.com/romiem"><FontAwesomeIcon icon={faGithub}/></a>
+    <a href="https://www.linkedin.com/in/rohitmistry"><FontAwesomeIcon icon={faLinkedin}/></a>
+    <a href="https://dribbble.com/romiem"><FontAwesomeIcon icon={faDribbble}/></a>
+    <a href="https://github.com/romiem"><FontAwesomeIcon icon={faGithub}/></a>
   </div>
 );
 
