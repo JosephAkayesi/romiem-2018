829445c39f7c8bf700b8c6a38398ab2b6df8ec9d
Rohit Mistry <romiem@Rohits-MacBook.local>
2018-03-09 19:15:05 +0000

    Fixed typo

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -7,7 +7,7 @@ const Description = () => (
     <h1>romiem</h1>
     <img className="Description__Logo" src={logo} alt="Romiem"/>
     <p>I’m a Product Designer and Developer with over 12 years of industry experience in crafting interfaces that users can intuitively understand and love.</p>
-    <p>I am based in London, UK — working as a UX Designer for Winton, having having previously worked for Microsoft and the BBC.</p>
+    <p>I am based in London, UK — working as a UX Designer for Winton, having previously worked for Microsoft and the BBC.</p>
     <p>My experience covers Visual &amp; Interaction design, Prototyping, Wireframing, Information Architecture, Animation and Coding.</p>
   </div>
 );
